51fd114a95bba8fc2892d759076a4bfb0cb2164c
Kim-ilhyeon <kih3915@naver.com>
2026-09-15 17:06:58 +0900

    practice code - 2026.09.15

--- react-profile-card/src/components/ProfileCard.jsx
+++ react-profile-card/src/components/ProfileCard.jsx
@@ -1,16 +1,52 @@
 import React from 'react'
 import './ProfileCard.css'
+import styled from 'styled-components'
 
-const ProfileCard = (datas) => {
+const ProfileCardLi = styled.li`
+  width: 200px;
+  padding: 20px;
+
+  border: 1px solid #ddd;
+  border-radius: 12px;
+
+  text-align: center;
+
+  box-shadow: 0 4px 10px rgba(0, 0, 0, 0.1);
+
+  transition: 0.3s;
+
+  &:hover {
+    transform: translateY(-5px);
+    box-shadow: 0 8px 16px rgba(0, 0, 0, 0.15);
+  }
+`
+const Online = styled.h3`
+  color: green;
+  font-weight: bold;
+`
+
+const Offline = styled.h3`
+  color: gray;
+  font-weight: bold;
+`
+
+const ProfileCard = ({datas}) => {
   return (
-    <div>
-        <ul>
-            {datas.map((data) => (
-                <li>{data.name}</li>
-                <li>{data.name}</li>
-            ))}
-        </ul>
-    </div>
+    <>
+      <div className='profile-container'>
+          <ul className='profile-list'>
+              {datas.map((data, index) => (
+                  <ProfileCardLi key={index}>
+                    <h2>{data.name}</h2>
+                    <h3>{data.age}세</h3>
+                    {data.isOnline ? 
+                      <Online >온라인</Online> : 
+                      <Offline >오프라인</Offline>}
+                  </ProfileCardLi>
+              ))}
+          </ul>
+      </div>
+    </>
   )
 }
 
