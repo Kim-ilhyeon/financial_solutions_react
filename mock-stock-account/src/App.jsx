26addaf43fd6e7d3635adcff79ff7a9ac9c5e56d
Kim-ilhyeon <kih3915@naver.com>
2026-09-16 17:40:52 +0900

    practice code -2026.09.16 PM

--- mock-stock-account/src/App.jsx
+++ mock-stock-account/src/App.jsx
@@ -1,18 +1,29 @@
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import heroImg from './assets/hero.png'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import './App.css'
-import { initialStocks } from './data/stocks'
+import { initialStocks, INITIAL_CASH } from './data/stocks'
+
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [stock, setStock] = useState(initialStocks);
+  const [cash, setCash] = useState(INITIAL_CASH);
+
+  handleRenewal = (price) => {
+    const rate = 1 + (Math.random() * 4 - 2) / 100;       // 0.98 ~ 1.02
+    const nextPrice = Math.round(price * rate / 10) * 10; // 10원 단위로 반올림
+    setStock(nextPrice);
+  }
+  // 종목별로 각각 랜덤값도 다르게 바뀌어야 함
 
   return (
     <>
-
+      <main>모의 투자 앱</main>
     </>
-  )
+  );
 }
 
 export default App
---
+++ mock-stock-account/src/components/StockRow.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const StockRow = () => {
+  return (
+    <div>StockRow</div>
+  )
+}
+
+export default StockRowfce
