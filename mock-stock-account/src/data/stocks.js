0cf16d56ca3efabee18be8f23b97034d4a629c9a
Kim-ilhyeon <kih3915@naver.com>
2026-09-16 14:00:56 +0900

    practice2 code -2026.09.16

---
+++ mock-stock-account/src/data/stocks.js
@@ -0,0 +1,9 @@
+// src/data/stocks.js
+export const initialStocks = [
+  { symbol: '005930', name: '삼성전자',   price: 71000 },
+  { symbol: '000660', name: 'SK하이닉스', price: 178000 },
+  { symbol: '035720', name: '카카오',     price: 42000 },
+  { symbol: '035420', name: 'NAVER',     price: 185000 },
+];
+
+export const INITIAL_CASH = 1000000; // 시작 현금 100만 원
